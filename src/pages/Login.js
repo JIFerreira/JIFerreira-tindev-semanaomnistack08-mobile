4fcc345b1b795aad59f7cdf68341d6a7e9bc0483
Jeferson Inacio Ferreira <jeferson.inacio.2@gmail.com>
2019-08-12 14:18:48 -0300

    enviando atualizações da branch de develop

---
+++ src/pages/Login.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Image,
+  TextInput,
+  Text,
+  Platform,
+  TouchableOpacity
+} from "react-native";
+
+import api from "../services/api";
+import logo from "../assets/logo.png";
+
+export default function Login({ navigation }) {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user => {
+      if (user) navigation.navigate("Main", { user });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function handleLogin() {
+    const response = await api.post("/devs", { username: user });
+
+    const { _id } = response.data;
+    await AsyncStorage.setItem("user", _id);
+    navigation.navigate("Main", { user: _id });
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled={Platform.OS === "ios"}
+      style={styles.container}
+    >
+      <Image source={logo} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholderTextColor="#999"
+        placeholder="Digite seu usuario no github"
+        value={user}
+        onChangeText={setUser}
+      />
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 30
+  },
+  input: {
+    height: 46,
+    alignSelf: "stretch",
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+  button: {
+    height: 46,
+    alignSelf: "stretch",
+    backgroundColor: "#DF4723",
+    borderRadius: 4,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16
+  }
+});
